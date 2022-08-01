e89738c0666647eee1b43eb066f53a2f5c552872
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-01 13:35:25 -0300

    feito splash page

--- stool-in-app-ui/lib/features/splash/presenter/page/splash_page.dart
+++ stool-in-app-ui/lib/features/splash/presenter/page/splash_page.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:stool_in_app_ui/core/constants/assets_constants.dart';
 
 class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
 
-  const SplashPage({ Key? key }) : super(key: key);
-
-   @override
-   Widget build(BuildContext context) {
-       return const Scaffold(
-           body: Center(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Spacer(),
+          Image.asset(AssetsConstants.logoWhite),
+          const Spacer(),
+          const CircularProgressIndicator.adaptive(
+            backgroundColor: Colors.white,
+          ),
+          const SizedBox(
+             height: 100,
+          ),
+        ],
+      ),
+    );
   }
 }
