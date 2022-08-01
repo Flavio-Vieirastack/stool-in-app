60f62f780da5f86abd3e4556413aa5b701acf906
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-01 14:17:42 -0300

    criado mixin de responsividade

--- stool-in-app-ui/lib/core/helpers/responsive/responsive_helper_mixin.dart
+++ stool-in-app-ui/lib/core/helpers/responsive/responsive_helper_mixin.dart
@@ -1,4 +1,46 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:stool_in_app_ui/core/helpers/responsive/app_sizes_constants.dart';
 
 mixin ResponsiveHelperMixin {
-  
+  num responsiveHeight({
+    required num defaultMobileHeight,
+    required num defaultMobileSmallSizeHeight,
+    required num defaultTabletHeight,
+    required BoxConstraints constraints,
+  }) {
+    if (constraints.maxWidth <= AppSizesContants.kMobileSmallBreakpoint) {
+      return defaultMobileSmallSizeHeight;
+    } else if (constraints.maxWidth > AppSizesContants.kMobileSmallBreakpoint &&
+        constraints.maxWidth <= AppSizesContants.kMobileBreakpoint) {
+      return defaultMobileHeight;
+    } else if (constraints.maxWidth > AppSizesContants.kMobileBreakpoint &&
+        constraints.maxWidth <= AppSizesContants.kTabletBreakpoint) {
+      log('default tablet size height loaded');
+      return defaultTabletHeight;
+    } else {
+      return defaultTabletHeight;
+    }
+  }
+
+  num responsiveWidth({
+    required num defaultMobileWidth,
+    required num defaultMobileSmallSizeWidth,
+    required num defaultTabletWidth,
+    required BoxConstraints constraints,
+  }) {
+    if (constraints.maxWidth <= AppSizesContants.kMobileSmallBreakpoint) {
+      return defaultMobileSmallSizeWidth;
+    } else if (constraints.maxWidth > AppSizesContants.kMobileSmallBreakpoint &&
+        constraints.maxWidth <= AppSizesContants.kMobileBreakpoint) {
+      return defaultMobileWidth;
+    } else if (constraints.maxWidth > AppSizesContants.kMobileBreakpoint &&
+        constraints.maxWidth <= AppSizesContants.kTabletBreakpoint) {
+      log('default tablet width loaded');
+      return defaultTabletWidth;
+    } else {
+      return defaultTabletWidth;
+    }
+  }
 }
