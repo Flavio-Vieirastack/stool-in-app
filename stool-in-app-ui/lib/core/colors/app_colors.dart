39e5a780de8820cab155e40e5789700c752a3eba
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-01 13:53:54 -0300

    configurado cores iniciais e criado classe de text styles

---
+++ stool-in-app-ui/lib/core/colors/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static Color buttonRightGradientColor = const Color(0xfffcaf24);
+  static Color buttonLeftGradientColor = const Color(0xfffcdd80);
+  static Color textFormFieldBackgroundColor = const Color(0xff424242);
+  static Color greyScaleDark = const Color(0xff2a2a2a);
+  static Color black = const Color(0xff060606);
+  static Color white = const Color(0xffffffff);
+  static Color grey = const Color(0xff969696);
+  static Color blackInnedRadial = const Color(0xff333132);
+}
--- stool-in-app-ui/lib/features/splash/presenter/page/splash_page.dart
+++ stool-in-app-ui/lib/features/splash/presenter/page/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stool_in_app_ui/core/colors/app_colors.dart';
 import 'package:stool_in_app_ui/core/constants/assets_constants.dart';
 
 class SplashPage extends StatelessWidget {
@@ -7,7 +8,7 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade900,
+      backgroundColor: AppColors.greyScaleDark,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
