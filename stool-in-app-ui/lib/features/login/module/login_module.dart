f1f9d1b8484efffa13f5bdc67445ddb0e5c52f60
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-02 10:58:28 -0300

    criado pagina de login e seu modulo e cubit

---
+++ stool-in-app-ui/lib/features/login/module/login_module.dart
@@ -0,0 +1,19 @@
+import 'package:provider/provider.dart';
+import 'package:stool_in_app_ui/core/constants/routes_constants.dart';
+import 'package:stool_in_app_ui/core/module/app_module.dart';
+import 'package:stool_in_app_ui/features/login/presenter/cubit/login_cubit.dart';
+import 'package:stool_in_app_ui/features/login/presenter/pages/login_page.dart';
+
+class LoginModule extends AppModule {
+  LoginModule()
+      : super(
+          dependencies: [
+            Provider(
+              create: (context) => LoginCubit(),
+            )
+          ],
+          routers: {
+            RoutesConstants.loginRoute: (context) => const LoginPage(),
+          },
+        );
+}
