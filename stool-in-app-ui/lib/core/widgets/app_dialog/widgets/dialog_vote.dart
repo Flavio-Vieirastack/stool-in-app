0e0ec19e298866bb4947075d29ceba0c58366ee4
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-02 14:56:57 -0300

    feito início do dialog de votos

--- stool-in-app-ui/lib/core/widgets/app_dialog/widgets/dialog_vote.dart
+++ stool-in-app-ui/lib/core/widgets/app_dialog/widgets/dialog_vote.dart
@@ -8,38 +8,42 @@ class _DialogVote extends StatefulWidget {
 }
 
 class _DialogVoteState extends State<_DialogVote> {
-  double value = 1;
+  double value = 0;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RatingStars(
-        value: value,
-        onValueChanged: (v) {
-          setState(() {
-            value = v;
-          });
-        },
-        starCount: 5,
-        starSize: 20,
-        valueLabelColor: const Color(0xff9b9b9b),
-        valueLabelTextStyle: const TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.w400,
-          fontStyle: FontStyle.normal,
-          fontSize: 12.0,
+    return Column(
+      children: [
+        Center(
+          child: RatingStars(
+            value: value,
+            onValueChanged: (v) {
+              setState(() {
+                value = v;
+              });
+            },
+            starCount: 5,
+            starSize: 20,
+            valueLabelColor: const Color(0xff9b9b9b),
+            valueLabelTextStyle: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w400,
+              fontStyle: FontStyle.normal,
+              fontSize: 12.0,
+            ),
+            valueLabelRadius: 10,
+            maxValue: 5,
+            starSpacing: 2,
+            maxValueVisibility: true,
+            valueLabelVisibility: true,
+            animationDuration: const Duration(milliseconds: 1000),
+            valueLabelPadding:
+                const EdgeInsets.symmetric(vertical: 1, horizontal: 8),
+            valueLabelMargin: const EdgeInsets.only(right: 8),
+            starOffColor: const Color(0xffe7e8ea),
+            starColor: Colors.yellow,
+          ),
         ),
-        valueLabelRadius: 10,
-        maxValue: 5,
-        starSpacing: 2,
-        maxValueVisibility: true,
-        valueLabelVisibility: true,
-        animationDuration: const Duration(milliseconds: 1000),
-        valueLabelPadding:
-            const EdgeInsets.symmetric(vertical: 1, horizontal: 8),
-        valueLabelMargin: const EdgeInsets.only(right: 8),
-        starOffColor: const Color(0xffe7e8ea),
-        starColor: Colors.yellow,
-      ),
+      ],
     );
   }
 }
