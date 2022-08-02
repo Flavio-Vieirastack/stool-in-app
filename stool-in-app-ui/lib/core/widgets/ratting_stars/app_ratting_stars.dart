39d5c1d7f49266c17681d29b1a801c3e976e919b
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-02 15:31:58 -0300

    refatorado text style do ratting star

--- stool-in-app-ui/lib/core/widgets/ratting_stars/app_ratting_stars.dart
+++ stool-in-app-ui/lib/core/widgets/ratting_stars/app_ratting_stars.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_stars/flutter_rating_stars.dart';
+import 'package:stool_in_app_ui/core/helpers/theme/text_styles/app_text_styles.dart';
 
 class AppRattingStars extends StatelessWidget {
   final double value;
   final double size;
+  final bool valueVisible;
   final dynamic Function(double)? onValueChanged;
   const AppRattingStars({
     Key? key,
     required this.value,
     required this.onValueChanged,
-    this.size = 40
+    this.size = 40,
+    this.valueVisible = false,
   }) : super(key: key);
 
   @override
@@ -20,17 +23,12 @@ class AppRattingStars extends StatelessWidget {
       starCount: 5,
       starSize: size,
       valueLabelColor: const Color(0xff9b9b9b),
-      valueLabelTextStyle: const TextStyle(
-        color: Colors.white,
-        fontWeight: FontWeight.w400,
-        fontStyle: FontStyle.normal,
-        fontSize: 12.0,
-      ),
+      valueLabelTextStyle: AppTextStyles.headLine3,
       valueLabelRadius: 10,
       maxValue: 5,
       starSpacing: 2,
-      maxValueVisibility: false,
-      valueLabelVisibility: false,
+      maxValueVisibility: valueVisible,
+      valueLabelVisibility: valueVisible,
       animationDuration: const Duration(milliseconds: 1000),
       valueLabelPadding: const EdgeInsets.symmetric(vertical: 1, horizontal: 8),
       valueLabelMargin: const EdgeInsets.only(right: 8),
