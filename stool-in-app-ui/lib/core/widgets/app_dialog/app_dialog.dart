683c9eedd2aa65b1a1bc994e387565725ff8f69d
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-02 14:52:47 -0300

    iniciado construção do dialog de votos

--- stool-in-app-ui/lib/core/widgets/app_dialog/app_dialog.dart
+++ stool-in-app-ui/lib/core/widgets/app_dialog/app_dialog.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_stars/flutter_rating_stars.dart';
 import 'package:lottie/lottie.dart';
 import 'package:stool_in_app_ui/core/helpers/theme/text_styles/app_text_styles.dart';
 import 'package:stool_in_app_ui/core/widgets/app_dialog/enum/dailog_types.dart';
@@ -10,6 +13,7 @@ part './widgets/dialog_no_connection.dart';
 part './widgets/dialog_info.dart';
 part './widgets/dialog_waiting.dart';
 part './widgets/dialog_blocked.dart';
+part './widgets/dialog_vote.dart';
 
 class AppDialog extends Dialog {
   final DialogTypes dailogTypes;
@@ -45,6 +49,8 @@ class AppDialog extends Dialog {
                   _DialogWaiting().waiting(message: 'message')
                 else if (dailogTypes == DialogTypes.blocked)
                   _DialogBlocked().blocked(message: 'message')
+                else if (dailogTypes == DialogTypes.voteRatting)
+                  const _DialogVote()
               ],
             ),
           ),
---
+++ stool-in-app-ui/lib/core/widgets/app_dialog/widgets/dialog_vote.dart
@@ -0,0 +1,45 @@
+part of '../app_dialog.dart';
+
+class _DialogVote extends StatefulWidget {
+  const _DialogVote({Key? key}) : super(key: key);
+
+  @override
+  State<_DialogVote> createState() => _DialogVoteState();
+}
+
+class _DialogVoteState extends State<_DialogVote> {
+  double value = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RatingStars(
+        value: value,
+        onValueChanged: (v) {
+          setState(() {
+            value = v;
+          });
+        },
+        starCount: 5,
+        starSize: 20,
+        valueLabelColor: const Color(0xff9b9b9b),
+        valueLabelTextStyle: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w400,
+          fontStyle: FontStyle.normal,
+          fontSize: 12.0,
+        ),
+        valueLabelRadius: 10,
+        maxValue: 5,
+        starSpacing: 2,
+        maxValueVisibility: true,
+        valueLabelVisibility: true,
+        animationDuration: const Duration(milliseconds: 1000),
+        valueLabelPadding:
+            const EdgeInsets.symmetric(vertical: 1, horizontal: 8),
+        valueLabelMargin: const EdgeInsets.only(right: 8),
+        starOffColor: const Color(0xffe7e8ea),
+        starColor: Colors.yellow,
+      ),
+    );
+  }
+}
