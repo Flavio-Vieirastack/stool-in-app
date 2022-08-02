4029dc28e1d3cdd55c4986abc8eb22d60a123eaf
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-02 16:26:49 -0300

    movido as dependencias permanente de pasta e adicionado novas constantes para rotas

--- stool-in-app-ui/lib/core/constants/routes_constants.dart
+++ stool-in-app-ui/lib/core/constants/routes_constants.dart
@@ -1,7 +1,13 @@
-
 class RoutesConstants {
   RoutesConstants._();
   static const String splashRoute = '/splash';
-  static const  String onBoardingRoute = '/onboarding';
-  static const  String loginRoute = '/login';
+  static const String onBoardingRoute = '/onboarding';
+  static const String loginRoute = '/login';
+  static const String signInMainRoute = '/sign-in-main';
+  static const String signInDataRoute = '/sign-in-data';
+  static const String passwordRecoveryRoute = '/password-recovery';
+  static const String homeRoute = '/home';
+  static const String serviceProviderOnBoardingRoute =
+      '/service-provider-onboarding';
+  static const String serviceProviderRoute = '/service-provider';
 }
--- stool-in-app-ui/lib/main.dart
+++ stool-in-app-ui/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:stool_in_app_ui/core/constants/routes_constants.dart';
-import 'package:stool_in_app_ui/core/permanent_dependencies/permanent_dependencies.dart';
 import 'package:stool_in_app_ui/features/login/module/login_module.dart';
 import 'package:stool_in_app_ui/features/onboarding/module/onboarding_module.dart';
 import 'package:stool_in_app_ui/features/splash/module/splash_module.dart';
+
+import 'core/module/permanent_dependencies/permanent_dependencies.dart';
 
 void main() {
   runApp(const MyApp());
