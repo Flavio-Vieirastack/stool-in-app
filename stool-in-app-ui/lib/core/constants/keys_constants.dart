b63abd9ef572bcd4fd9c1001378d1ccc9842fc31
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-02 16:38:24 -0300

    adicionado keys principais para preferencias

--- stool-in-app-ui/lib/core/constants/keys_constants.dart
+++ stool-in-app-ui/lib/core/constants/keys_constants.dart
@@ -1,4 +1,8 @@
 class KeysConstants {
   KeysConstants._();
   static const onboardingFlowKey = 'onboarding-flow';
+  static const signInDataFlowKey = 'sign-in-data-flow';
+  static const userToken = 'user-token';
+  static const userFirebaseToken = 'user-firebase-token';
+  static const userFirebasePushToken = 'user-firebase-push-token';
 }
