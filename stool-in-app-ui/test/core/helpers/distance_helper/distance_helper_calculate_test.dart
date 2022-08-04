718905b1b6f5a4dac90e7055c6cd7bd8a6bc6311
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-04 16:20:23 -0300

    feito rowback para o package de ui

--- stool-in-app-ui/test/core/helpers/distance_helper/distance_helper_calculate_test.dart
+++ stool-in-app-ui/test/core/helpers/distance_helper/distance_helper_calculate_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:haversine_distance/haversine_distance.dart';
-import 'package:stool_in_app_logic/core/helpers/distance_helper/distance_helper_calculate.dart';
+import 'package:stool_in_app_ui/core/helpers/distance_helper/distance_helper_calculate.dart';
 
 void main() {
   late HaversineDistance haversineDistance;
