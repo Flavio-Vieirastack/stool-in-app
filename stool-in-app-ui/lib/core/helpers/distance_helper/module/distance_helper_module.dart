323f05e0e36db58c6aafad2edf00e2d64dff2516
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-04 16:18:49 -0300

    commit pontual

--- stool-in-app-ui/lib/core/helpers/distance_helper/module/distance_helper_module.dart
+++ stool-in-app-ui/lib/core/helpers/distance_helper/module/distance_helper_module.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:haversine_distance/haversine_distance.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
-import 'package:stool_in_app_ui/core/helpers/distance_helper/distance_helper_calculate.dart';
+import 'package:stool_in_app_logic/core/helpers/distance_helper/distance_helper_calculate.dart';
 import 'package:stool_in_app_ui/core/module/main_module/inject.dart';
 @immutable
 class DistanceHelperModule {
