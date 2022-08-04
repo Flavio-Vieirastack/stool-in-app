87d804a167934187bad019104c1fe3fef9aa2fa3
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-04 16:26:26 -0300

    movido o distance helper

--- stool-in-app-ui/lib/core/helpers/distance_helper/module/distance_helper_module.dart
+++ stool-in-app-ui/lib/core/helpers/distance_helper/module/distance_helper_module.dart
@@ -1,10 +1,13 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:haversine_distance/haversine_distance.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
-import 'package:stool_in_app_ui/core/module/main_module/inject.dart';
+import 'package:stool_in_app_logic/core/helpers/distance_helper/distance_helper_calculate.dart';
 
-import '../distance_helper_calculate.dart';
+import '../../../module/main_module/inject.dart';
+
 @immutable
 class DistanceHelperModule {
   List<SingleChildWidget> module() {
