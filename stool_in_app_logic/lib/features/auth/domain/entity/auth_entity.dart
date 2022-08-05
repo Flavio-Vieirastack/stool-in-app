f10dc70e1658c447e0740de2982032a0a0d17006
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-05 12:55:49 -0300

    criado entidade para auth

---
+++ stool_in_app_logic/lib/features/auth/domain/entity/auth_entity.dart
@@ -0,0 +1,23 @@
+class AuthEntity {
+  final String email;
+  final String password;
+  final String? firebaseUuid;
+  AuthEntity({
+    required this.email,
+    required this.password,
+    this.firebaseUuid,
+  });
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is AuthEntity &&
+      other.email == email &&
+      other.password == password &&
+      other.firebaseUuid == firebaseUuid;
+  }
+
+  @override
+  int get hashCode => email.hashCode ^ password.hashCode ^ firebaseUuid.hashCode;
+}
