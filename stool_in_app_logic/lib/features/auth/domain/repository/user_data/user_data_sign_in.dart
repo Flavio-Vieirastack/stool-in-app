b1b7b92fbd5823196f1fe0c1a644a2e103bf91e6
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-05 16:37:38 -0300

    criado repository para user data

--- stool_in_app_logic/lib/features/auth/domain/repository/user_data/user_data_sign_in.dart
+++
@@ -1,5 +0,0 @@
-import 'package:stool_in_app_logic/stool_in_app_logic.dart';
-
-abstract class UserDataSignIn {
-  
-}
---
+++ stool_in_app_logic/lib/features/auth/domain/repository/user_data/user_data_sign_in_respository.dart
@@ -0,0 +1,9 @@
+import 'package:stool_in_app_logic/features/auth/domain/entity/user_data_entity.dart';
+import 'package:stool_in_app_logic/features/auth/domain/error/user_data_error.dart';
+import 'package:stool_in_app_logic/stool_in_app_logic.dart';
+
+abstract class UserDataSignInRepository {
+  Future<Either<UserDataError, UserDataEntity>> call({
+    required UserDataEntity userDataEntity,
+  });
+}
