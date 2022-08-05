d40d2bd2ebf7c84442b88935a7743795e58eeacf
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-05 13:22:03 -0300

    feito inicio do repository para auth

--- stool_in_app_logic/lib/features/auth/data/datasource/auth_datasource.dart
+++ stool_in_app_logic/lib/features/auth/data/datasource/auth_datasource.dart
@@ -1,3 +1,6 @@
+import 'package:stool_in_app_logic/features/auth/data/model/auth_model.dart';
+
 abstract class AuthDatasource {
-  
+  Future<void> apiAuth({required AuthModel authModel});
+  Future<void> firebaseAuth({required AuthModel authModel});
 }
---
+++ stool_in_app_logic/lib/features/auth/data/repository/auth_repository_impl.dart
@@ -0,0 +1,56 @@
+import 'dart:developer';
+
+import 'package:stool_in_app_logic/features/auth/data/datasource/auth_datasource.dart';
+import 'package:stool_in_app_logic/features/auth/data/model/auth_model.dart';
+import 'package:stool_in_app_logic/features/auth/domain/error/firebase_auth_error.dart';
+import 'package:stool_in_app_logic/features/auth/domain/error/api_auth_error.dart';
+import 'package:stool_in_app_logic/features/auth/domain/entity/auth_entity.dart';
+import 'package:dartz/dartz.dart';
+import 'package:stool_in_app_logic/features/auth/domain/repository/auth_repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthDatasource _authDatasource;
+  AuthRepositoryImpl({
+    required AuthDatasource authDatasource,
+  }) : _authDatasource = authDatasource;
+  @override
+  Future<Either<ApiAuthError, void>> apiAuth({
+    required AuthEntity authEntity,
+  }) async {
+    try {
+      final result = await _authDatasource.apiAuth(
+        authModel: AuthModel.fromEntity(authEntity),
+      );
+      return Right(result);
+    } on ApiAuthError catch (e, s) {
+      log(
+        'Erro ao realizar login para a api',
+        error: e,
+        stackTrace: s,
+      );
+      return Left(
+        ApiAuthError(
+          message: e.message,
+        ),
+      );
+    } catch (e, s) {
+      log(
+        'Erro deconhecido ao realizar login para a api',
+        error: e,
+        stackTrace: s,
+      );
+      return Left(
+        ApiAuthError(
+          message: e.toString(),
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<FirebaseAuthError, void>> firebaseAuth(
+      {required AuthEntity authEntity}) {
+    // TODO: implement firebaseAuth
+    throw UnimplementedError();
+  }
+}
