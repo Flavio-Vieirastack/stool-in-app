c9d20eda6718c2b335afb35f44594e135e460f72
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-05 20:28:58 -0300

    feito teste para o datasource

---
+++ stool_in_app_logic/test/features/auth/data/datasource/user_data/user_data_sign_in_datasource_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:stool_in_app_logic/features/auth/data/datasource/user_data/user_data_sign_in_datasource.dart';
+import 'package:stool_in_app_logic/features/auth/data/model/user_data_model.dart';
+import 'package:stool_in_app_logic/features/auth/domain/entity/user_data_entity.dart';
+
+class UseDataSignInDataSourceMock extends Mock
+    implements UserDataSignInDatasource {}
+
+void main() {
+  test('Deve retornar o tipo correto', () async {
+    final mock = UseDataSignInDataSourceMock();
+    final modelMock = UserDataModel();
+    when(
+      () => mock.sendUserData(userDataModel: modelMock),
+    ).thenAnswer((_) async => UserDataEntity());
+    final sut = await mock.sendUserData(userDataModel: modelMock);
+    expect(sut, isA<UserDataEntity>());
+  });
+}
