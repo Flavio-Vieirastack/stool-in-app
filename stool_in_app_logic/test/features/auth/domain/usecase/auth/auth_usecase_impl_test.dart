724d451182f8736dbdf6e99db683ce98dd12c1df
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-05 17:23:59 -0300

    feito teste para o repository

--- stool_in_app_logic/test/features/auth/domain/usecase/auth/auth_usecase_impl_test.dart
+++ stool_in_app_logic/test/features/auth/domain/usecase/auth/auth_usecase_impl_test.dart
@@ -1,13 +1,25 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:stool_in_app_logic/features/auth/domain/entity/user_data_entity.dart';
+import 'package:stool_in_app_logic/features/auth/domain/repository/login/login_repository.dart';
+import 'package:stool_in_app_logic/features/auth/domain/repository/password_reset/password_reset_repository.dart';
+import 'package:stool_in_app_logic/features/auth/domain/repository/sign_in/sign_in_repository.dart';
 import 'package:stool_in_app_logic/features/auth/domain/repository/user_data/user_data_sign_in_respository.dart';
 import 'package:stool_in_app_logic/features/auth/domain/usecase/auth/auth_use_case.dart';
+import 'package:stool_in_app_logic/features/auth/domain/usecase/auth/auth_usecase_impl.dart';
 import 'package:stool_in_app_logic/stool_in_app_logic.dart';
 
 class AuthMock extends Mock implements AuthUseCase {}
 
 class RepositoryMock extends Mock implements UserDataSignInRepository {}
+
+class LoginRepositoryMock extends Mock implements LoginRepository {}
+
+class SignInRepositoryMock extends Mock implements SignInRepository {}
+
+class PasswordRepositoryMock extends Mock implements PasswordResetRepository {}
+
+class UserDataSignInMock extends Mock implements UserDataSignInRepository {}
 
 void main() {
   test('Deve retornar a entidade correta ao ser chamado', () async {
@@ -24,20 +36,27 @@ void main() {
     expect(sut, Right(entity));
   });
   test('Deve chamar o repository ao ser chamado', () async {
-    final useCaseMock = AuthMock();
     final entity = UserDataEntity();
-    final repository = RepositoryMock();
+    final loginRepositoryMock = LoginRepositoryMock();
+    final signInRepositoryMock = SignInRepositoryMock();
+    final passwordResetRepository = PasswordRepositoryMock();
+    final userDataSignInMock = UserDataSignInMock();
+    final useCaseImpl = AuthUsecaseImpl(
+        loginRepository: loginRepositoryMock,
+        signInRepository: signInRepositoryMock,
+        passwordResetRepository: passwordResetRepository,
+        userDataSignInRepository: userDataSignInMock);
     when(
-      () => useCaseMock.sendUserData(userDataEntity: entity),
+      () => userDataSignInMock.sendUserData(userDataEntity: entity),
     ).thenAnswer(
       (_) async => Right(
-        UserDataEntity(),
+        entity,
       ),
     );
-    final sut = await useCaseMock.sendUserData(userDataEntity: entity);
-    expect(sut, Right(entity));
+    final sut = await useCaseImpl.sendUserData(userDataEntity: entity);
     verify(
-      () => repository.sendUserData(userDataEntity: entity),
+      () => userDataSignInMock.sendUserData(userDataEntity: entity),
     ).called(1);
+    expect(sut, Right(entity));
   });
 }
