3c9d6fe1d2dec6bbb039167be881acab2f3c6703
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-05 11:08:20 -0300

    movido o internet helper

--- stool_in_app_logic/lib/stool_in_app_logic.dart
+++ stool_in_app_logic/lib/stool_in_app_logic.dart
@@ -3,3 +3,5 @@ library stool_in_app_logic;
 export 'package:google_fonts/google_fonts.dart';
 export 'package:haversine_distance/haversine_distance.dart';
 export './core/helpers/distance_helper/distance_helper_calculate.dart';
+export 'package:internet_connection_checker/internet_connection_checker.dart';
+export './core/helpers/internet_helper/dialog_button_internet_checker.dart';
