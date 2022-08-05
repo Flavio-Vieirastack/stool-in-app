36e35e0464913e34689dcb9a4a2506f820cf2548
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-05 15:50:57 -0300

    ajustado identação do datasource

--- stool_in_app_logic/lib/features/auth/data/datasource/login/login_datasource.dart
+++ stool_in_app_logic/lib/features/auth/data/datasource/login/login_datasource.dart
@@ -1,6 +1,10 @@
 import 'package:stool_in_app_logic/features/auth/data/model/auth_model.dart';
 
 abstract class LoginDatasource {
-  Future<void> apiLogin({required AuthModel authModel});
-  Future<void> firebaseLogin({required AuthModel authModel});
+  Future<void> apiLogin({
+    required AuthModel authModel,
+  });
+  Future<void> firebaseLogin({
+    required AuthModel authModel,
+  });
 }
