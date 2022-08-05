a557ddeb00b94333368d916aa241be01efc06ff5
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-05 16:56:42 -0300

    criado implementação do repository para user data

---
+++ stool_in_app_logic/lib/features/auth/data/repository/user_data/user_data_sign_in_repository_impl.dart
@@ -0,0 +1,35 @@
+import 'dart:developer';
+
+import 'package:stool_in_app_logic/features/auth/data/datasource/user_data/user_data_sign_in_datasource.dart';
+import 'package:stool_in_app_logic/features/auth/data/model/user_data_model.dart';
+import 'package:stool_in_app_logic/features/auth/domain/error/user_data_error.dart';
+import 'package:stool_in_app_logic/features/auth/domain/entity/user_data_entity.dart';
+import 'package:dartz/dartz.dart';
+import 'package:stool_in_app_logic/features/auth/domain/repository/user_data/user_data_sign_in_respository.dart';
+
+class UserDataSignInRepositoryImpl implements UserDataSignInRepository {
+  final UserDataSignInDatasource _userDataSignInDatasource;
+  UserDataSignInRepositoryImpl({
+    required UserDataSignInDatasource userDataSignInDatasource,
+  }) : _userDataSignInDatasource = userDataSignInDatasource;
+  @override
+  Future<Either<UserDataError, UserDataEntity>> sendUserData({
+    required UserDataEntity userDataEntity,
+  }) async {
+    try {
+      final result = await _userDataSignInDatasource.sendUserData(
+        userDataModel: UserDataModel.fromEntity(userDataEntity: userDataEntity),
+      );
+      return Right(result);
+    } on UserDataError catch (e, s) {
+      log(
+        'Erro ao enviar dados do usuário para a api',
+        error: e,
+        stackTrace: s,
+      );
+      return Left(
+        UserDataError(message: e.message),
+      );
+    }
+  }
+}
