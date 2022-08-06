c5602575dbcf5cd2580c510ec66297442e900f33
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-06 15:07:50 -0300

    feito ajuste para tela pequena do iphone

--- stool-in-app-ui/lib/features/onboarding/presenter/page/widgets/onboarding_pages.dart
+++ stool-in-app-ui/lib/features/onboarding/presenter/page/widgets/onboarding_pages.dart
@@ -74,7 +74,7 @@ class OnboardingPages extends StatelessWidget with ResponsiveHelperMixin {
               visible: showButton,
               child: Center(
                 child: Padding(
-                  padding: const EdgeInsets.only(top: 600),
+                  padding: const EdgeInsets.only(top: 600), //!Small
                   child: AppButton(
                     buttonText: 'Eu quero',
                     onPressed: onPressed,
