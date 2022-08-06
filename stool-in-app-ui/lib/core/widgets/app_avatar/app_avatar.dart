10e4b853b56129eea4b81dc42c848d4209c528aa
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-06 17:48:48 -0300

    feito ajustes no avatar

--- stool-in-app-ui/lib/core/widgets/app_avatar/app_avatar.dart
+++ stool-in-app-ui/lib/core/widgets/app_avatar/app_avatar.dart
@@ -1,14 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:stool_in_app_ui/core/helpers/theme/colors/app_colors.dart';
 
 class AppAvatar extends StatelessWidget {
-  final String urlImage;
+  final String? urlImage;
   final double size;
-  const AppAvatar({
-    Key? key,
-    required this.urlImage,
-    this.size = 50
-  }) : super(key: key);
+  const AppAvatar({Key? key, this.urlImage, this.size = 50}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,13 +14,22 @@ class AppAvatar extends StatelessWidget {
       child: SizedBox(
         height: size,
         width: size,
-        child: CachedNetworkImage(
-          imageUrl: urlImage,
-          fit: BoxFit.cover,
-          progressIndicatorBuilder: (context, url, downloadProgress) =>
-              CircularProgressIndicator(value: downloadProgress.progress),
-          errorWidget: (context, url, error) => const Icon(Icons.person),
-        ),
+        child: urlImage != null
+            ? CachedNetworkImage(
+                imageUrl: urlImage ?? '',
+                fit: BoxFit.cover,
+                progressIndicatorBuilder: (context, url, downloadProgress) =>
+                    CircularProgressIndicator(value: downloadProgress.progress),
+                errorWidget: (context, url, error) => const Icon(Icons.person),
+              )
+            : CircleAvatar(
+                backgroundColor: AppColors.grey,
+                child: Icon(
+                  Icons.person,
+                  size: 50,
+                  color: AppColors.black,
+                ),
+              ),
       ),
     );
   }
--- stool-in-app-ui/lib/features/auth/sign_in_user_data/presenter/page/sign_in_main_data_page.dart
+++ stool-in-app-ui/lib/features/auth/sign_in_user_data/presenter/page/sign_in_main_data_page.dart
@@ -1,14 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:stool_in_app_ui/core/helpers/responsive/responsive_helper_mixin.dart';
 
-class SignInMainDataPage extends StatelessWidget {
+import '../../../../../core/helpers/theme/colors/app_colors.dart';
+import '../../../../../core/helpers/theme/text_styles/app_text_styles.dart';
+import '../../../../../core/widgets/app_avatar/app_avatar.dart';
 
-  const SignInMainDataPage({ Key? key }) : super(key: key);
+class SignInMainDataPage extends StatefulWidget {
+  const SignInMainDataPage({Key? key}) : super(key: key);
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('data'),),
-           body: Container(),
-       );
+  @override
+  State<SignInMainDataPage> createState() => _SignInMainDataPageState();
+}
+
+class _SignInMainDataPageState extends State<SignInMainDataPage>
+    with ResponsiveHelperMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.grey.withOpacity(0.1),
+      resizeToAvoidBottomInset: false,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return ListView(
+            children: [
+              SizedBox(
+                height: constraints.maxHeight,
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: constraints.maxHeight *
+                            responsiveHeight(
+                              defaultMobileHeight: 0.1,
+                              defaultMobileSmallSizeHeight: 0.2,
+                              defaultTabletHeight: 0.2,
+                              constraints: constraints,
+                            ),
+                        left: constraints.maxWidth *
+                            responsiveWidth(
+                              defaultMobileWidth: 0.05,
+                              defaultMobileSmallSizeWidth: 0.01,
+                              defaultTabletWidth: 0.01,
+                              constraints: constraints,
+                            ),
+                      ),
+                      child: Text(
+                        'Bem vindo(a)',
+                        style: AppTextStyles.headLine0,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        bottom: constraints.maxHeight *
+                            responsiveHeight(
+                              defaultMobileHeight: 0.05,
+                              defaultMobileSmallSizeHeight: 0.8,
+                              defaultTabletHeight: 0.8,
+                              constraints: constraints,
+                            ),
+                      ),
+                      child: Center(child: Container()),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        bottom: constraints.maxHeight *
+                            responsiveHeight(
+                              defaultMobileHeight: 0.5,
+                              defaultMobileSmallSizeHeight: 0.2,
+                              defaultTabletHeight: 0.2,
+                              constraints: constraints,
+                            ),
+                      ),
+                      child: const Center(
+                        child: AppAvatar(
+                          size: 100,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
