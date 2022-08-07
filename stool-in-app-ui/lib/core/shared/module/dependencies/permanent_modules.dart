bff017dc0d3ab0ab8bf8c900463db73198fd7c1a
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-07 14:09:12 -0300

    feito injeção do firebase auth

--- stool-in-app-ui/lib/core/shared/module/dependencies/permanent_modules.dart
+++ stool-in-app-ui/lib/core/shared/module/dependencies/permanent_modules.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/single_child_widget.dart';
+import 'package:stool_in_app_ui/core/firebase/firebase_auth/module/firebase_auth_module.dart';
 import 'package:stool_in_app_ui/core/helpers/distance_helper/module/distance_helper_module.dart';
 import 'package:stool_in_app_ui/core/helpers/secure_storage_helper/module/secure_storage_module.dart';
 import 'package:stool_in_app_ui/core/helpers/vote_ratting/module/vote_ratting_module.dart';
@@ -10,6 +11,7 @@ class PermanentModules {
       ...SecureStorageModule().inject(),
       ...DistanceHelperModule().inject(),
       ...VoteRattingModule().inject(),
+      ...FirebaseAuthModule().inject(),
     ];
   }
 }
--- stool-in-app-ui/lib/features/auth/sign_in/module/sign_in_main_module.dart
+++ stool-in-app-ui/lib/features/auth/sign_in/module/sign_in_main_module.dart
@@ -1,10 +1,17 @@
+import 'package:provider/provider.dart';
 import 'package:stool_in_app_logic/core/constants/routes_constants.dart';
 import 'package:stool_in_app_ui/core/module/main_module/app_module.dart';
+import 'package:stool_in_app_ui/features/auth/sign_in/presenter/cubit/sign_in_cubit.dart';
 import 'package:stool_in_app_ui/features/auth/sign_in/presenter/page/sign_in_main_page.dart';
 
 class SignInMainModule extends AppModule {
   SignInMainModule()
       : super(
+          dependencies: [
+            Provider<SignInCubit>(
+              create: (context) => SignInCubit(),
+            )
+          ],
           routers: {
             RoutesConstants.signInMainRoute: (context) => const SignInMainPage()
           },
