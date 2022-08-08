40a11fa282354550931959ea5169837f0f35eaa6
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-08 17:20:11 -0300

    avançado mais nos testes do model

--- stool-in-app-ui/lib/features/home/data/model/execution_services_model.dart
+++ stool-in-app-ui/lib/features/home/data/model/execution_services_model.dart
@@ -2,9 +2,7 @@ import 'dart:convert';
 
 import 'package:stool_in_app_ui/features/home/domain/entity/execution_services_entity.dart';
 
-
 class ExecutionServicesModel extends ExecutionServicesEntity {
-  
   ExecutionServicesModel({
     required super.id,
     required super.serviceName,
@@ -15,27 +13,29 @@ class ExecutionServicesModel extends ExecutionServicesEntity {
 
   Map<String, dynamic> toMap() {
     final result = <String, dynamic>{};
-  
-    result.addAll({'ida': id});
-    result.addAll({'serviceNamea': serviceName});
-    result.addAll({'pricea': price});
-    result.addAll({'estimatedHoursa': estimatedHours});
-    result.addAll({'serviceProviderIda': serviceProviderId});
-  
+
+    result.addAll({'id': id});
+    result.addAll({'serviceName': serviceName});
+    result.addAll({'price': price});
+    result.addAll({'estimatedHours': estimatedHours});
+    result.addAll({'serviceProviderId': serviceProviderId});
+
     return result;
   }
 
   factory ExecutionServicesModel.fromMap(Map<String, dynamic> map) {
+    final price = double.tryParse(map['price']);
     return ExecutionServicesModel(
-      id: map['ida']?.toInt() ?? 0,
-      serviceName: map['serviceNamea'] ?? '',
-      price: map['pricea']?.toDouble() ?? 0.0,
-      estimatedHours: map['estimatedHoursa']?.toInt() ?? 0,
-      serviceProviderId: map['serviceProviderIda']?.toInt() ?? 0,
+      id: map['id']?.toInt() ?? 0,
+      serviceName: map['serviceName'] ?? '',
+      price: price ?? 0.0,
+      estimatedHours: map['estimatedHours']?.toInt() ?? 0,
+      serviceProviderId: map['serviceProviderId']?.toInt() ?? 0,
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory ExecutionServicesModel.fromJson(String source) => ExecutionServicesModel.fromMap(json.decode(source));
+  factory ExecutionServicesModel.fromJson(String source) =>
+      ExecutionServicesModel.fromMap(json.decode(source));
 }
--- stool-in-app-ui/test/features/home/data/model/service_provider_model_test.dart
+++ stool-in-app-ui/test/features/home/data/model/service_provider_model_test.dart
@@ -10,4 +10,54 @@ void main() {
         .toList();
     expect(model[0].id, 1);
   });
+  test('Deve retornar a descrição corretamente', () {
+    final model = serviceProviderPayload
+        .map((e) => ServiceProviderModel.fromMap(e))
+        .toList();
+    expect(model[0].serviceProviderDescription,
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s");
+  });
+  test('Deve retornar o status corretamente', () {
+    final model = serviceProviderPayload
+        .map((e) => ServiceProviderModel.fromMap(e))
+        .toList();
+    expect(model[0].status, "ATIVO");
+  });
+  test('Deve retornar o o horario inicial corretamente', () {
+    final model = serviceProviderPayload
+        .map((e) => ServiceProviderModel.fromMap(e))
+        .toList();
+    expect(model[0].initialDisponibleTime, "06:00");
+  });
+  test('Deve retornar o o horario final corretamente', () {
+    final model = serviceProviderPayload
+        .map((e) => ServiceProviderModel.fromMap(e))
+        .toList();
+    expect(model[0].endDisponibleTime, "17:30");
+  });
+  test('Deve retornar o os dias da semana corretamente', () {
+    final model = serviceProviderPayload
+        .map((e) => ServiceProviderModel.fromMap(e))
+        .toList();
+    expect(model[0].disponibleDays, "segunda a sexta");
+  });
+  test('Deve retornar a data de criação corretamente em date time', () {
+    final model = serviceProviderPayload
+        .map((e) => ServiceProviderModel.fromMap(e))
+        .toList();
+    expect(model[0].createdAt, DateTime.parse("2022-08-08 15:44:02.468Z"));
+  });
+  test('Deve retornar o user data id corretamente', () {
+    final model = serviceProviderPayload
+        .map((e) => ServiceProviderModel.fromMap(e))
+        .toList();
+    expect(model[0].userDataId, 1);
+  });
+  test('Deve retornar o id do execution services corretamente', () {
+    final model = serviceProviderPayload
+        .map((e) => ServiceProviderModel.fromMap(e))
+        .toList();
+    final sut = model[0].executionServices;
+    expect(sut[0].id, 1);
+  });
 }
