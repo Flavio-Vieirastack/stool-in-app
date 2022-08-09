4451d51c73ff3329cecda421da1c3d218792e00b
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-09 14:30:00 -0300

    feito injeções para login

--- stool-in-app-ui/lib/features/auth/presenter/login/cubit/login_cubit.dart
+++ stool-in-app-ui/lib/features/auth/presenter/login/cubit/login_cubit.dart
@@ -1,10 +1,12 @@
-
-
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stool_in_app_ui/features/auth/domain/usecase/auth/auth_use_case.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginInitial());
+  final AuthUseCase _authUseCase;
+  LoginCubit({required AuthUseCase authUseCase})
+      : _authUseCase = authUseCase,
+        super(LoginInitial());
 }
