b463985aa17efe0f44eff567c1d26b4d6c1b3bea
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-09 17:59:29 -0300

    feito testes para lotties

---
+++ stool-in-app-ui/test/core/constants/lottie_constants_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stool_in_app_ui/core/constants/lottie_constants.dart';
+
+void main() {
+  test('Deve retornar o lottie para lottieSucess', () {
+    const lottie = LottieConstants.lottieSucess;
+    const lottieName = 'assets/lotties/31975-successful-message.json';
+    expect(lottie, lottieName);
+  });
+  test('Deve retornar o lottie para lottieError', () {
+    const lottie = LottieConstants.lottieError;
+    const lottieName = 'assets/lotties/97670-tomato-error.json';
+    expect(lottie, lottieName);
+  });
+  test('Deve retornar o lottie para lottieNoConnection', () {
+    const lottie = LottieConstants.lottieNoConnection;
+    const lottieName = 'assets/lotties/84872-disconnected.json';
+    expect(lottie, lottieName);
+  });
+  test('Deve retornar o lottie para lottieWaiting', () {
+    const lottie = LottieConstants.lottieWaiting;
+    const lottieName = 'assets/lotties/21421-waiting.json';
+    expect(lottie, lottieName);
+  });
+  test('Deve retornar o lottie para lottieBlcoked', () {
+    const lottie = LottieConstants.lottieBlcoked;
+    const lottieName = 'assets/lotties/76732-locked-icon.json';
+    expect(lottie, lottieName);
+  });
+}
