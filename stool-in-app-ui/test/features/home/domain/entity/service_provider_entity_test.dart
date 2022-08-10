a32d08be443c1828cea489479cc2a6b22a1e23c6
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-10 13:26:34 -0300

    feito teste para a entidade de service provider

---
+++ stool-in-app-ui/test/features/home/domain/entity/service_provider_entity_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:stool_in_app_ui/features/auth/domain/entity/user_data_entity.dart';
+import 'package:stool_in_app_ui/features/home/domain/entity/coments_entity.dart';
+import 'package:stool_in_app_ui/features/home/domain/entity/execution_services_entity.dart';
+import 'package:stool_in_app_ui/features/home/domain/entity/service_provider_entity.dart';
+import 'package:stool_in_app_ui/features/home/domain/entity/services_to_execute_entity.dart';
+
+class ExectionServicesMock extends Mock implements ExecutionServicesEntity {}
+
+class ServicesToExecuteMock extends Mock implements ServicesToExecuteEntity {}
+
+class ComentsMock extends Mock implements ComentsEntity {}
+
+class UserDataMock extends Mock implements UserDataEntity {}
+
+void main() {
+  late ServiceProviderEntity serviceProviderEntity;
+  setUp(() {
+    serviceProviderEntity = ServiceProviderEntity(
+      id: 1,
+      distance: -435,
+      serviceProviderDescription: 'serviceProviderDescription',
+      status: 'status',
+      initialDisponibleTime: 'initialDisponibleTime',
+      endDisponibleTime: 'endDisponibleTime',
+      disponibleDays: 'disponibleDays',
+      createdAt: DateTime.now(),
+      userDataId: 1,
+      executionServices: [ExectionServicesMock()],
+      servicesToExecute: [ServicesToExecuteMock()],
+      coments: [ComentsMock()],
+      userData: UserDataMock(),
+    );
+  });
+  test('Deve retornar a distância formatada corretamente em KM', () {
+    final result = serviceProviderEntity.distanceFormat();
+    const matcher = '1 Km';
+    expect(result, matcher);
+  });
+  test('Deve retornar a distância formatada corretamente em Metros', () {
+    final result = serviceProviderEntity.distanceFormat();
+    const matcher = '435 M';
+    expect(result, matcher);
+  });
+}
