d81c8022a6aa286109893568372a104436932f72
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-11 14:54:44 -0300

    criado teste para service status

---
+++ stool-in-app-ui/test/core/enum/service_status_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stool_in/core/enum/service_status.dart';
+
+void main() {
+  test('Deve retornar o status correto para active', () {
+    const sut = ServiceStatus.active;
+    const matcher = ServiceStatus.active;
+    expect(sut, matcher);
+  });
+  test('Deve retornar o status correto para inactive', () {
+    const sut = ServiceStatus.inactive;
+    const matcher = ServiceStatus.inactive;
+    expect(sut, matcher);
+  });
+}
