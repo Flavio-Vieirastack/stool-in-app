4f8a5f467c0e5ed1f45c5831cf5d7ba9000ab9b8
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-11 13:18:02 -0300

    feito refactor na estrutura de pastas

--- stool-in-app-ui/lib/core/core_modules/firebase_auth/firebase_auth_module.dart
+++ stool-in-app-ui/lib/core/core_modules/firebase_auth/firebase_auth_module.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
-import '../../../module/permanent_dependencies/permanent_module.dart';
+import '../../module/permanent_dependencies/permanent_module.dart';
 
 class FirebaseAuthModule implements PermanentModule {
   @override
--- stool-in-app-ui/lib/core/shared/module/dependencies/permanent_modules.dart
+++ stool-in-app-ui/lib/core/shared/module/dependencies/permanent_modules.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:stool_in/core/core_modules/firebase_storage/firebase_storage_module.dart';
-import 'package:stool_in/core/firebase/firebase_auth/module/firebase_auth_module.dart';
 import 'package:stool_in/core/firebase/push_notifications/module/firebase_notifications_module.dart';
 import 'package:stool_in/core/helpers/distance_helper/module/distance_helper_module.dart';
 import 'package:stool_in/core/helpers/pick_image_helper/module/pick_image_helper_module.dart';
 import 'package:stool_in/core/helpers/secure_storage_helper/module/secure_storage_module.dart';
 import 'package:stool_in/core/helpers/vote_ratting/module/vote_ratting_module.dart';
+
+import '../../../core_modules/firebase_auth/firebase_auth_module.dart';
 
 @immutable
 class PermanentModules {
