846ab12f450b50633c3d80e253ab96a16b13e1eb
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-11 11:20:29 -0300

    criado classe de pick image

---
+++ stool-in-app-ui/lib/core/helpers/pick_image_helper/pick_image_helper.dart
@@ -0,0 +1,24 @@
+import 'package:image_picker/image_picker.dart';
+
+enum ImageFrom {
+  camera,
+  gallery,
+}
+
+class PickImageHelper {
+  final ImagePicker _imagePicker;
+  PickImageHelper({
+    required ImagePicker imagePicker,
+  }) : _imagePicker = imagePicker;
+  Future<XFile?> getImage({required ImageFrom imageFrom}) async {
+    if (imageFrom == ImageFrom.camera) {
+      final XFile? image =
+          await _imagePicker.pickImage(source: ImageSource.camera);
+      return image;
+    } else {
+      final XFile? image =
+          await _imagePicker.pickImage(source: ImageSource.gallery);
+      return image;
+    }
+  }
+}
