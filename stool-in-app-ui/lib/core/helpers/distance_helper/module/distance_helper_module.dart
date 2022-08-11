cb12d28ba502a943764b98499dd81990b030d5a4
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-11 11:23:28 -0300

    ijetado o distance helper

--- stool-in-app-ui/lib/core/helpers/distance_helper/module/distance_helper_module.dart
+++ stool-in-app-ui/lib/core/helpers/distance_helper/module/distance_helper_module.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
-import '../../../module/main_module/inject.dart';
 import '../../../module/permanent_dependencies/permanent_module.dart';
 import '../distance_helper_calculate.dart';
-
 
 @immutable
 class DistanceHelperModule implements PermanentModule {
   @override
   List<SingleChildWidget> inject() {
     return [
-     
+      Provider(
+        create: (context) => DistanceHelperCalculate(),
+      )
     ];
   }
 }
