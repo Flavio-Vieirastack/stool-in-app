16b534b1b355297540f555b30318d2548bcca98e
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-12 20:36:32 -0300

    refatorado testes cabiveis

--- stool-in-app-ui/test/features/auth/data/datasource/login/login_datasource_impl_test.dart
+++ stool-in-app-ui/test/features/auth/data/datasource/login/login_datasource_impl_test.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:stool_in/core/rest_client/rest_client_contracts.dart';
@@ -21,7 +23,7 @@ void main() {
   late AuthModel authModel;
   late Map<String, dynamic> response;
   setUp(
-    () {
+    () async {
       authModel = AuthModel(email: 'email', password: 'password');
       response = {'access_token': 'token'};
       userTokenEntity = UserTokenEntity(token: 'token');
@@ -33,40 +35,44 @@ void main() {
       );
     },
   );
-  test('Deve retornar uma entidade de token corretamente', () async {
-    when(
-      () => postImplMock.post(
-        path: any(named: 'path'),
-        data: any(named: 'data'),
-      ),
-    ).thenAnswer(
-        (_) async => RestClientResponse(data: response, statucCode: 200));
-    final sut = await loginDatasource.apiLogin(authModel: authModel);
-    expect(sut, userTokenEntity);
-  });
-  test('Deve retornar chamar o post corretamente', () async {
-    when(
-      () => postImplMock.post(
-        path: any(named: 'path'),
-        data: any(named: 'data'),
-      ),
-    ).thenAnswer(
-        (_) async => RestClientResponse(data: response, statucCode: 200));
-    final sut = await loginDatasource.apiLogin(authModel: authModel);
-    expect(sut, userTokenEntity);
-    verify(
-      () =>
-          postImplMock.post(path: any(named: 'path'), data: any(named: 'data')),
-    ).called(1);
-  });
-  test('Deve retornar uma erro corretamente', () async {
-    when(
-      () => postImplMock.post(
-        path: any(named: 'path'),
-        data: any(named: 'data'),
-      ),
-    ).thenThrow(ApiAuthError(message: 'Erro ao fazer login, tente mais tarde'));
-    final sut = loginDatasource.apiLogin;
-    expect(() async => sut(authModel: authModel), throwsException);
+
+  group('api tests', () {
+    test('Deve retornar uma entidade de token corretamente', () async {
+      when(
+        () => postImplMock.post(
+          path: any(named: 'path'),
+          data: any(named: 'data'),
+        ),
+      ).thenAnswer(
+          (_) async => RestClientResponse(data: response, statucCode: 200));
+      final sut = await loginDatasource.apiLogin(authModel: authModel);
+      expect(sut, userTokenEntity);
+    });
+    test('Deve retornar chamar o post corretamente', () async {
+      when(
+        () => postImplMock.post(
+          path: any(named: 'path'),
+          data: any(named: 'data'),
+        ),
+      ).thenAnswer(
+          (_) async => RestClientResponse(data: response, statucCode: 200));
+      final sut = await loginDatasource.apiLogin(authModel: authModel);
+      expect(sut, userTokenEntity);
+      verify(
+        () => postImplMock.post(
+            path: any(named: 'path'), data: any(named: 'data')),
+      ).called(1);
+    });
+    test('Deve retornar uma erro corretamente', () async {
+      when(
+        () => postImplMock.post(
+          path: any(named: 'path'),
+          data: any(named: 'data'),
+        ),
+      ).thenThrow(
+          ApiAuthError(message: 'Erro ao fazer login, tente mais tarde'));
+      final sut = loginDatasource.apiLogin;
+      expect(() async => sut(authModel: authModel), throwsException);
+    });
   });
 }
