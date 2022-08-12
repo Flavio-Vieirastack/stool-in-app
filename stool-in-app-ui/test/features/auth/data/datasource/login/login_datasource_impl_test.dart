a759dd73bafb61fc5f3d950211cca5eb14105d1a
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-12 17:38:03 -0300

    refatorado teste para login datasource

--- stool-in-app-ui/test/features/auth/data/datasource/login/login_datasource_impl_test.dart
+++ stool-in-app-ui/test/features/auth/data/datasource/login/login_datasource_impl_test.dart
@@ -1,36 +1,46 @@
-import 'package:faker/faker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:stool_in/core/rest_client/rest_client_contracts.dart';
+import 'package:stool_in/core/rest_client/rest_client_response.dart';
 import 'package:stool_in/features/auth/data/datasource/login/login_datasource.dart';
+import 'package:stool_in/features/auth/data/datasource/login/login_datasource_impl.dart';
 import 'package:stool_in/features/auth/data/model/auth_model.dart';
 import 'package:stool_in/features/auth/domain/entity/user_token_entity.dart';
-import 'package:stool_in/features/auth/domain/error/api_auth_error.dart';
 
-class LoginDatasourceMock extends Mock implements LoginDatasource {}
+class PostImplMock extends Mock implements RestClientPost {}
+
+class FirebaseAuthMock extends Mock implements FirebaseAuth {}
 
 void main() {
-  test('Deve retornar o token corretamente', () async {
-    final faker = Faker();
-    final email = faker.internet.email();
-    final token = faker.jwt.valid();
-    final loginDataMock = LoginDatasourceMock();
-    final authModel = AuthModel(email: email);
-    final entity = UserTokenEntity(token: token);
+  late LoginDatasource loginDatasource;
+  late PostImplMock postImplMock;
+  late FirebaseAuthMock firebaseAuthMock;
+  late UserTokenEntity userTokenEntity;
+  late AuthModel authModel;
+  late Map<String, dynamic> response;
+  setUp(
+    () {
+      authModel = AuthModel(email: 'email', password: 'password');
+      response = {'access_token': 'token'};
+      userTokenEntity = UserTokenEntity(token: 'token');
+      firebaseAuthMock = FirebaseAuthMock();
+      postImplMock = PostImplMock();
+      loginDatasource = LoginDatasourceImpl(
+        restClientPost: postImplMock,
+        firebaseAuth: firebaseAuthMock,
+      );
+    },
+  );
+  test('Deve retornar uma entidade de token corretamente', () async {
     when(
-      () => loginDataMock.apiLogin(authModel: authModel),
-    ).thenAnswer((_) async => entity);
-    final sut = await loginDataMock.apiLogin(authModel: authModel);
-    expect(sut, entity);
-  });
-  test('Deve retornar um erro corretamente', () async {
-    final faker = Faker();
-    final email = faker.internet.email();
-    final loginDataMock = LoginDatasourceMock();
-    final authModel = AuthModel(email: email);
-    when(
-      () => loginDataMock.apiLogin(authModel: authModel),
-    ).thenThrow(ApiAuthError(message: 'message'));
-    final sut = loginDataMock.apiLogin;
-    expect(() async => sut(authModel: authModel), throwsException);
+      () => postImplMock.post(
+        path: any(named: 'path'),
+        data: any(named: 'data'),
+      ),
+    ).thenAnswer(
+        (_) async => RestClientResponse(data: response, statucCode: 200));
+    final sut = await loginDatasource.apiLogin(authModel: authModel);
+    expect(sut, userTokenEntity);
   });
 }
