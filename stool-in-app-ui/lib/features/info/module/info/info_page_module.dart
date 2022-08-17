80c8071dc639beeb740b5961497b81e8a5777e5d
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-17 13:52:24 -0300

    movido a pasta info

--- stool-in-app-ui/lib/features/info/module/info/info_page_module.dart
+++ stool-in-app-ui/lib/features/info/module/info/info_page_module.dart
@@ -1,6 +1,6 @@
 import 'package:stool_in/core/constants/routes_constants.dart';
 import 'package:stool_in/core/module/main_module/app_module.dart';
-import 'package:stool_in/features/info/presenter/page/info_page.dart';
+import 'package:stool_in/features/info/presenter/page/info/info_page.dart';
 
 class InfoPageModule extends AppModule {
   InfoPageModule()
--- stool-in-app-ui/lib/main.dart
+++ stool-in-app-ui/lib/main.dart
@@ -12,7 +12,7 @@ import 'package:stool_in/features/auth/presenter/password_reset/module/password_
 import 'package:stool_in/features/auth/presenter/sign_in/module/sign_in_main_module.dart';
 import 'package:stool_in/features/auth/presenter/sign_in_user_data/module/sign_in_main_data_module.dart';
 import 'package:stool_in/features/home/presenter/module/home_module.dart';
-import 'package:stool_in/features/info/module/info_page_module.dart';
+import 'package:stool_in/features/info/module/info/info_page_module.dart';
 import 'package:stool_in/features/onboarding/module/onboarding_module.dart';
 import 'package:stool_in/features/splash/module/splash_module.dart';
 
