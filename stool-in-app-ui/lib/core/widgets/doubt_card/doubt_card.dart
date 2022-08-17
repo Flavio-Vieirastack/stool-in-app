61804091de7cb1fd3d3876ee737e3a7b85841cac
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-17 14:34:39 -0300

    feito pagina de duvidas

--- stool-in-app-ui/lib/core/widgets/doubt_card/doubt_card.dart
+++ stool-in-app-ui/lib/core/widgets/doubt_card/doubt_card.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
-import 'package:stool_in/core/helpers/theme/colors/app_colors.dart';
 import 'package:stool_in/core/helpers/theme/text_styles/app_text_styles.dart';
 
 class DoubtCard extends StatelessWidget {
-  const DoubtCard({Key? key}) : super(key: key);
+  final String title;
+  final String content;
+  const DoubtCard({
+    Key? key,
+    required this.content,
+    required this.title,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ExpansionTile(
         title: Text(
-          'Birth of Universe',
+          title,
           style: AppTextStyles.headLine3Gold,
         ),
         children: <Widget>[
           Padding(
             padding: EdgeInsets.all(8.0.dp),
             child: Text(
-              'but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+              content,
               style: AppTextStyles.headLine4,
             ),
           ),
--- stool-in-app-ui/lib/features/info/presenter/page/doubt/doubt_page.dart
+++ stool-in-app-ui/lib/features/info/presenter/page/doubt/doubt_page.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:stool_in/core/widgets/doubt_card/doubt_card.dart';
+import 'package:flutter_sizer/flutter_sizer.dart';
 
 class DoubtPage extends StatelessWidget {
   const DoubtPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final fixedLengthList =
+        List<int>.generate(10, (int index) => index * index, growable: false);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Dúvidas'),
       ),
-      body: Column(
-        children: const [
-          DoubtCard(),
-        ],
+      body: Padding(
+        padding: EdgeInsets.all(8.0.dp),
+        child: ListView(
+          children: fixedLengthList
+              .map(
+                (e) => Padding(
+                  padding:  EdgeInsets.only(top: 0.5.h, bottom: 0.5.h),
+                  child: const DoubtCard(
+                    content:
+                        'but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                    title: 'Duvida X',
+                  ),
+                ),
+              )
+              .toList(),
+        ),
       ),
     );
   }
