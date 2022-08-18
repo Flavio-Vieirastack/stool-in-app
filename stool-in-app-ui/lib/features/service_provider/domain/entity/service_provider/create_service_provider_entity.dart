83f627cbd6ee3896546c7b5b1c5893c30b08b7e9
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-18 17:34:41 -0300

    modificado entidade de create service provider

--- stool-in-app-ui/lib/features/service_provider/domain/entity/service_provider/create_service_provider_entity.dart
+++ stool-in-app-ui/lib/features/service_provider/domain/entity/service_provider/create_service_provider_entity.dart
@@ -1,13 +1,17 @@
 class CreateServiceProviderEntity {
   final int? id;
+  final int? userLoginId;
   final String userDescription;
   final int votes;
   final String status;
   final DateTime initialDisponibleTime;
   final DateTime endDisponibleTime;
+  final DateTime? createdAt;
   final String disponibleDays;
   CreateServiceProviderEntity({
     this.id,
+    this.userLoginId,
+    this.createdAt,
     required this.userDescription,
     required this.votes,
     required this.status,
@@ -15,29 +19,32 @@ class CreateServiceProviderEntity {
     required this.endDisponibleTime,
     required this.disponibleDays,
   });
-
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-
+  
     return other is CreateServiceProviderEntity &&
-        other.id == id &&
-        other.userDescription == userDescription &&
-        other.votes == votes &&
-        other.status == status &&
-        other.initialDisponibleTime == initialDisponibleTime &&
-        other.endDisponibleTime == endDisponibleTime &&
-        other.disponibleDays == disponibleDays;
+      other.id == id &&
+      other.userLoginId == userLoginId &&
+      other.userDescription == userDescription &&
+      other.votes == votes &&
+      other.status == status &&
+      other.initialDisponibleTime == initialDisponibleTime &&
+      other.endDisponibleTime == endDisponibleTime &&
+      other.createdAt == createdAt &&
+      other.disponibleDays == disponibleDays;
   }
 
   @override
   int get hashCode {
     return id.hashCode ^
-        userDescription.hashCode ^
-        votes.hashCode ^
-        status.hashCode ^
-        initialDisponibleTime.hashCode ^
-        endDisponibleTime.hashCode ^
-        disponibleDays.hashCode;
+      userLoginId.hashCode ^
+      userDescription.hashCode ^
+      votes.hashCode ^
+      status.hashCode ^
+      initialDisponibleTime.hashCode ^
+      endDisponibleTime.hashCode ^
+      createdAt.hashCode ^
+      disponibleDays.hashCode;
   }
 }
