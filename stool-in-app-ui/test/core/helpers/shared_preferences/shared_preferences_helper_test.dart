964611eb9df73938d2bd20fa99be0e8c79082e48
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-21 14:18:53 -0300

    feito teste para o shared preferences

--- stool-in-app-ui/test/core/helpers/shared_preferences/shared_preferences_helper_test.dart
+++ stool-in-app-ui/test/core/helpers/shared_preferences/shared_preferences_helper_test.dart
@@ -1,54 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:stool_in/core/helpers/shared_preferences/shared_preferences_helper.dart';
 
-class PreferencesMock extends Mock implements SharedPreferencesHelper {}
+class MockMixin with SharedPreferencesHelper {}
 
 void main() {
-  test('Deve retornar o valor correto ao pegar um bool', () async {
+  late MockMixin mockMixin;
+  setUp(() {
+    mockMixin = MockMixin();
+  });
+
+  test(
+      'Deve retornar o valor booleano correto como true para o shared preferences',
+      () async {
     WidgetsFlutterBinding.ensureInitialized();
-    final mock = PreferencesMock();
-    when(
-      () => mock.getBool(key: 'any()'),
-    ).thenAnswer((invocation) async => true);
-    final sut = await mock.getBool(key: 'any()');
+    SharedPreferences.setMockInitialValues({});
+    await mockMixin.saveBool(key: 'teste', value: true);
+    final sut = await mockMixin.getBool(key: 'teste');
+
     expect(sut, true);
   });
-  test('Deve retornar o valor correto ao pegar uma string', () async {
+  test(
+      'Deve retornar o valor booleano correto como false para o shared preferences',
+      () async {
     WidgetsFlutterBinding.ensureInitialized();
-    final mock = PreferencesMock();
-    when(
-      () => mock.getString(key: 'any()'),
-    ).thenAnswer((invocation) async => 'true');
-    final sut = await mock.getString(key: 'any()');
-    expect(sut, 'true');
+    SharedPreferences.setMockInitialValues({});
+    await mockMixin.saveBool(key: 'teste', value: false);
+    final sut = await mockMixin.getBool(key: 'teste');
+
+    expect(sut, false);
   });
-  test('Deve retornar o valor correto ao pegar um int', () async {
+  test(
+      'Deve retornar o valor double correto para o shared preferences',
+      () async {
     WidgetsFlutterBinding.ensureInitialized();
-    final mock = PreferencesMock();
-    when(
-      () => mock.getInt(key: 'any()'),
-    ).thenAnswer((invocation) async => 1);
-    final sut = await mock.getInt(key: 'any()');
+    SharedPreferences.setMockInitialValues({});
+    await mockMixin.saveDouble(key: 'teste', value: 2.0);
+    final sut = await mockMixin.getDouble(key: 'teste');
+
+    expect(sut, 2.0);
+  });
+  test(
+      'Deve retornar o valor int correto para o shared preferences',
+      () async {
+    WidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences.setMockInitialValues({});
+    await mockMixin.saveInt(key: 'teste', value: 1);
+    final sut = await mockMixin.getInt(key: 'teste');
+
     expect(sut, 1);
   });
-  test('Deve retornar o valor correto ao pegar um double', () async {
+  test(
+      'Deve retornar o valor string correto para o shared preferences',
+      () async {
     WidgetsFlutterBinding.ensureInitialized();
-    final mock = PreferencesMock();
-    when(
-      () => mock.getDouble(key: 'any()'),
-    ).thenAnswer((invocation) async => 1.5);
-    final sut = await mock.getDouble(key: 'any()');
-    expect(sut, 1.5);
+    SharedPreferences.setMockInitialValues({});
+    await mockMixin.saveString(key: 'teste', value: 'teste');
+    final sut = await mockMixin.getString(key: 'teste');
+
+    expect(sut, 'teste');
   });
-  test('Deve retornar o valor correto ao pegar uma lista de strings', () async {
+  test(
+      'Deve retornar o valor string list correto para o shared preferences',
+      () async {
     WidgetsFlutterBinding.ensureInitialized();
-    final mock = PreferencesMock();
-    when(
-      () => mock.getStringList(key: 'any()'),
-    ).thenAnswer((invocation) async => ['any()', 'any()']);
-    final sut = await mock.getStringList(key: 'any()');
-    expect(sut, isNotEmpty);
+    SharedPreferences.setMockInitialValues({});
+    await mockMixin.saveStringList(key: 'teste', value: ['teste', 'teste2']);
+    final sut = await mockMixin.getStringList(key: 'teste');
+
+    expect(sut?.length, 2);
   });
 }
