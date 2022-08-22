ce47ff898e0c9337d365ca885883809bdc06a1a3
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-22 16:44:17 -0300

    refatorado testes do rules

--- stool-in-app-ui/test/features/info/data/datasource/rules/rules_datasource_impl_test.dart
+++ stool-in-app-ui/test/features/info/data/datasource/rules/rules_datasource_impl_test.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:stool_in/core/cache/save_json_in_cache_datasource.dart';
 import 'package:stool_in/core/rest_client/rest_client_contracts.dart';
 import 'package:stool_in/core/rest_client/rest_client_response.dart';
 import 'package:stool_in/features/info/data/datasource/rules/rules_datasource.dart';
@@ -8,21 +11,28 @@ import 'package:stool_in/features/info/domain/entity/info_entity.dart';
 import 'package:stool_in/features/info/domain/error/info_error.dart';
 
 class RestclientGetMock extends Mock implements RestClientGet {}
-
+class SaveJsonCacheMock extends Mock implements SaveJsonInCacheDatasource {}
 void main() {
   late RestclientGetMock restclientGetMock;
   late RulesDatasource rulesDatasource;
+  late SaveJsonCacheMock saveJsonCacheMock;
   late List<InfoEntity> infoEntity;
   late List<Map<String, dynamic>> response;
   setUp(() {
     infoEntity = [InfoEntity(id: 1, title: 'title', body: 'body')];
     restclientGetMock = RestclientGetMock();
+    saveJsonCacheMock = SaveJsonCacheMock();
     rulesDatasource = RulesDatasourceImpl(restClientGet: restclientGetMock);
     response = [
       {"id": 1, "title": "title", "body": "body"}
     ];
   });
   test('Deve retornar uma lista de info entity', () async {
+    WidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences.setMockInitialValues({});
+    when(
+      () => saveJsonCacheMock.saveJsonInCache(data: 'data', key: 'key'),
+    ).thenAnswer((_) async => Future.value());
     when(
       () => restclientGetMock.get(path: any(named: 'path')),
     ).thenAnswer((_) async => RestClientResponse(
@@ -33,6 +43,11 @@ void main() {
     expect(sut[0], infoEntity[0]);
   });
   test('Deve chamar o restclient get', () async {
+    WidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences.setMockInitialValues({});
+    when(
+      () => saveJsonCacheMock.saveJsonInCache(data: 'data', key: 'key'),
+    ).thenAnswer((_) async => Future.value());
     when(
       () => restclientGetMock.get(path: any(named: 'path')),
     ).thenAnswer((_) async => RestClientResponse(
@@ -46,6 +61,11 @@ void main() {
     ).called(1);
   });
   test('Deve retornar um erro', () async {
+    WidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences.setMockInitialValues({});
+    when(
+      () => saveJsonCacheMock.saveJsonInCache(data: 'data', key: 'key'),
+    ).thenAnswer((_) async => Future.value());
     when(
       () => restclientGetMock.get(path: any(named: 'path')),
     ).thenThrow(InfoError(message: 'message'));
