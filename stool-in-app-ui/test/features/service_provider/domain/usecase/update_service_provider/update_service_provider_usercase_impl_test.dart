d739b4c724533c13fcb5462194fb85938fa4badc
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-24 13:20:19 -0300

    feito teste para usecase de update do service provider

---
+++ stool-in-app-ui/test/features/service_provider/domain/usecase/update_service_provider/update_service_provider_usercase_impl_test.dart
@@ -0,0 +1,73 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:stool_in/features/home/domain/error/service_provider/service_provider_error.dart';
+import 'package:stool_in/features/service_provider/domain/entity/service_provider/create_and_update_service_provider_entity.dart';
+import 'package:stool_in/features/service_provider/domain/repository/update_service_provider_repository/update_service_provider_repository.dart';
+import 'package:stool_in/features/service_provider/domain/usecase/update_service_provider/update_service_provider_usecase.dart';
+import 'package:stool_in/features/service_provider/domain/usecase/update_service_provider/update_service_provider_usercase_impl.dart';
+
+class UpdateServiceProviderRepositoryMock extends Mock
+    implements UpdateServiceProviderRepository {}
+
+class CreateAndUpdateServiceProviderEntityMock extends Mock
+    implements CreateAndUpdateServiceProviderEntity {}
+
+void main() {
+  late UpdateServiceProviderRepositoryMock updateServiceProviderRepositoryMock;
+  late CreateAndUpdateServiceProviderEntityMock
+      createAndUpdateServiceProviderEntityMock;
+  late UpdateServiceProviderUsecase updateServiceProviderUsecase;
+  setUp(() {
+    updateServiceProviderRepositoryMock = UpdateServiceProviderRepositoryMock();
+    createAndUpdateServiceProviderEntityMock =
+        CreateAndUpdateServiceProviderEntityMock();
+    updateServiceProviderUsecase = UpdateServiceProviderUsecaseImpl(
+        updateServiceProviderRepository: updateServiceProviderRepositoryMock);
+  });
+  test(
+      'Deve retornar void ao chamar o usecase para dar update no service provider',
+      () async {
+    when(
+      () => updateServiceProviderRepositoryMock.call(
+          createAndUpdateServiceProviderEntity:
+              createAndUpdateServiceProviderEntityMock),
+    ).thenAnswer((_) async => const Right(null));
+    final sut = await updateServiceProviderUsecase.call(
+        createAndUpdateServiceProviderEntity:
+            createAndUpdateServiceProviderEntityMock);
+    expect(sut, const Right(null));
+  });
+  test(
+      'Deve chamar o repository ao retornar void quando chamar o usecase para dar update no service provider',
+      () async {
+    when(
+      () => updateServiceProviderRepositoryMock.call(
+          createAndUpdateServiceProviderEntity:
+              createAndUpdateServiceProviderEntityMock),
+    ).thenAnswer((_) async => const Right(null));
+    final sut = await updateServiceProviderUsecase.call(
+        createAndUpdateServiceProviderEntity:
+            createAndUpdateServiceProviderEntityMock);
+    expect(sut, const Right(null));
+    verify(
+      () => updateServiceProviderRepositoryMock.call(
+          createAndUpdateServiceProviderEntity:
+              createAndUpdateServiceProviderEntityMock),
+    ).called(1);
+  });
+  test(
+      'Deve retornar um erro do tipo esperado ao chamar o update servie provider',
+      () async {
+    when(
+      () => updateServiceProviderRepositoryMock.call(
+          createAndUpdateServiceProviderEntity:
+              createAndUpdateServiceProviderEntityMock),
+    ).thenAnswer((_) async => Left(ServiceProviderError(message: '')));
+    final sut = await updateServiceProviderUsecase.call(
+        createAndUpdateServiceProviderEntity:
+            createAndUpdateServiceProviderEntityMock);
+    expect(sut, Left(ServiceProviderError(message: '')));
+    
+  });
+}
