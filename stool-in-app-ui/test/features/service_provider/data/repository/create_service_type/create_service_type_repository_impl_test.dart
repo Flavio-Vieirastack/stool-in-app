f32e03a94eb41c19a0b85501fcd75bffce627694
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-24 16:17:13 -0300

    criado testes para o repository de criar um service type

---
+++ stool-in-app-ui/test/features/service_provider/data/repository/create_service_type/create_service_type_repository_impl_test.dart
@@ -0,0 +1,70 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:stool_in/features/service_provider/data/datasource/create_service_type/create_service_type_datasource.dart';
+import 'package:stool_in/features/service_provider/data/model/service_type/create_service_type_model.dart';
+import 'package:stool_in/features/service_provider/data/repository/create_service_type/create_service_type_repository_impl.dart';
+import 'package:stool_in/features/service_provider/domain/entity/service_types/create_service_types_entity.dart';
+import 'package:stool_in/features/service_provider/domain/entity/service_types/service_types_return_entity.dart';
+import 'package:stool_in/features/service_provider/domain/error/create_service_type_error.dart';
+import 'package:stool_in/features/service_provider/domain/repository/create_service_type/create_service_type_repository.dart';
+
+class CreateServiceTypesDatasourceMock extends Mock
+    implements CreateServiceTypeDatasource {}
+
+class CreateServiceTyeReturnEntityMock extends Mock
+    implements ServiceTypesReturnEntity {}
+
+void main() {
+  late CreateServiceTypesDatasourceMock createServiceTypesDatasourceMock;
+  late CreateServiceTypeRepository createServiceTypeRepository;
+  late CreateServiceTypesEntity createServiceTypeEntityMock;
+  late CreateServiceTyeReturnEntityMock createServiceTyeReturnEntityMock;
+  setUpAll(() {
+    createServiceTyeReturnEntityMock = CreateServiceTyeReturnEntityMock();
+    createServiceTypeEntityMock =
+        CreateServiceTypesEntity(serviceProviderId: 1);
+    createServiceTypesDatasourceMock = CreateServiceTypesDatasourceMock();
+    createServiceTypeRepository = CreateServiceTypeRepositoryImpl(
+        createServiceTypeDatasource: createServiceTypesDatasourceMock);
+    registerFallbackValue(CreateServiceTypeModel.fromEntity(
+        createServiceTypesEntity: createServiceTypeEntityMock));
+  });
+  test('Deve retornar a entidade de criação de service type corretamente',
+      () async {
+    when(
+      () => createServiceTypesDatasourceMock.call(
+        createServiceTypeModel: any(named: 'createServiceTypeModel'),
+      ),
+    ).thenAnswer((_) async => createServiceTyeReturnEntityMock);
+    final sut = await createServiceTypeRepository.call(
+        createServiceTypesEntity: createServiceTypeEntityMock);
+    expect(sut, Right(createServiceTyeReturnEntityMock));
+  });
+  test(
+      'Deve chamar o datasource ao retornar a entidade de criação de service type corretamente',
+      () async {
+    when(
+      () => createServiceTypesDatasourceMock.call(
+        createServiceTypeModel: any(named: 'createServiceTypeModel'),
+      ),
+    ).thenAnswer((_) async => createServiceTyeReturnEntityMock);
+    final sut = await createServiceTypeRepository.call(
+        createServiceTypesEntity: createServiceTypeEntityMock);
+    expect(sut, Right(createServiceTyeReturnEntityMock));
+    verify(
+      () => createServiceTypesDatasourceMock.call(
+          createServiceTypeModel: any(named: 'createServiceTypeModel')),
+    ).called(1);
+  });
+  test('Deve retornar um erro ao criar um service type', () async {
+    when(
+      () => createServiceTypesDatasourceMock.call(
+        createServiceTypeModel: any(named: 'createServiceTypeModel'),
+      ),
+    ).thenThrow(CreateServiceTypeError(message: 'message'));
+    final sut = await createServiceTypeRepository.call(
+        createServiceTypesEntity: createServiceTypeEntityMock);
+    expect(sut, Left(CreateServiceTypeError(message: 'message')));
+  });
+}
