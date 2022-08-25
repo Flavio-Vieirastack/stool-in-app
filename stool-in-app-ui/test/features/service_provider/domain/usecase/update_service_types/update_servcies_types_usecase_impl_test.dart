33fda3b7910f76df3bfed0c856a813bd03fe13b6
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-25 11:53:49 -0300

    feito testes para uscacase do update de service types

--- stool-in-app-ui/test/features/service_provider/domain/usecase/update_service_types/update_servcies_types_usecase_impl_test.dart
+++ stool-in-app-ui/test/features/service_provider/domain/usecase/update_service_types/update_servcies_types_usecase_impl_test.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:stool_in/features/service_provider/domain/entity/service_types/create_service_types_entity.dart';
+import 'package:stool_in/features/service_provider/domain/error/update_service_types_error.dart';
 import 'package:stool_in/features/service_provider/domain/repository/update_service_types/update_service_types_repository.dart';
 import 'package:stool_in/features/service_provider/domain/usecase/update_service_types/update_servcies_types_usecase_impl.dart';
 import 'package:stool_in/features/service_provider/domain/usecase/update_service_types/update_service_types_usecase.dart';
@@ -31,5 +32,30 @@ void main() {
     final sut = await updateServiceTypesUsecase.call(
         createServiceTypesEntity: createServiceTypeEntityMock);
     expect(sut, const Right(null));
+  });
+  test(
+      'Deve chamar o repository ao retornar void ao fazer update do service type com sucesso',
+      () async {
+    when(
+      () => updateServiceTypeRepositoryMock.call(
+          createServiceTypesEntity: createServiceTypeEntityMock),
+    ).thenAnswer((_) async => const Right(null));
+    final sut = await updateServiceTypesUsecase.call(
+        createServiceTypesEntity: createServiceTypeEntityMock);
+    expect(sut, const Right(null));
+    verify(
+      () => updateServiceTypeRepositoryMock.call(
+          createServiceTypesEntity: createServiceTypeEntityMock),
+    ).called(1);
+  });
+  test('Deve retornar um erro ao fazer update do service type com sucesso',
+      () async {
+    when(
+      () => updateServiceTypeRepositoryMock.call(
+          createServiceTypesEntity: createServiceTypeEntityMock),
+    ).thenAnswer((_) async => Left(UpdateServiceTypesError(message: '')));
+    final sut = await updateServiceTypesUsecase.call(
+        createServiceTypesEntity: createServiceTypeEntityMock);
+    expect(sut, Left(UpdateServiceTypesError(message: '')));
   });
 }
