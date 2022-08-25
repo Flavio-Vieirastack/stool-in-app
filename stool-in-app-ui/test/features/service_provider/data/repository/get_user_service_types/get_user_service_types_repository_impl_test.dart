ca57f45d385136a81ce1c695c3bf200d6b5fe86c
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-25 15:35:42 -0300

    feito teste para repository do get service types

---
+++ stool-in-app-ui/test/features/service_provider/data/repository/get_user_service_types/get_user_service_types_repository_impl_test.dart
@@ -0,0 +1,67 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:stool_in/features/service_provider/data/datasource/get_user_service_types/get_user_service_types_datasource.dart';
+import 'package:stool_in/features/service_provider/data/repository/get_user_service_types/get_user_service_types_repository_impl.dart';
+import 'package:stool_in/features/service_provider/domain/entity/service_types/create_service_types_entity.dart';
+import 'package:stool_in/features/service_provider/domain/entity/service_types/service_types_return_entity.dart';
+import 'package:stool_in/features/service_provider/domain/error/get_user_service_types_error.dart';
+import 'package:stool_in/features/service_provider/domain/repository/get_user_service_types/get_user_service_types_repository.dart';
+
+class _GetUserServiceTypesDatasourceMock extends Mock
+    implements GetUserServiceTypesDatasource {}
+
+class _CreateServiceTypeEntityMock extends Mock
+    implements CreateServiceTypesEntity {}
+
+class _ServiceTypesReturnEntityMock extends Mock
+    implements ServiceTypesReturnEntity {}
+
+void main() {
+  late _GetUserServiceTypesDatasourceMock getUserServiceTypesDatasourceMock;
+  late List<_ServiceTypesReturnEntityMock> serviceTypesReturnEntityMock;
+  late _CreateServiceTypeEntityMock createServiceTypeEntityMock;
+  late GetUserServiceTypesRepository getUserServiceTypesRepository;
+  setUp(() {
+    getUserServiceTypesDatasourceMock = _GetUserServiceTypesDatasourceMock();
+    serviceTypesReturnEntityMock = [_ServiceTypesReturnEntityMock()];
+    createServiceTypeEntityMock = _CreateServiceTypeEntityMock();
+    getUserServiceTypesRepository = GetUserServiceTypesRepositoryImpl(
+        getUserServiceTypesDatasource: getUserServiceTypesDatasourceMock);
+  });
+  test(
+      'Deve retornar uma lista de entidade correta ao fazer get do service types',
+      () async {
+    when(
+      () => getUserServiceTypesDatasourceMock.call(
+          serviceProviderId: createServiceTypeEntityMock),
+    ).thenAnswer((_) async => serviceTypesReturnEntityMock);
+    final sut = await getUserServiceTypesRepository.call(
+        serviceProviderId: createServiceTypeEntityMock);
+    expect(sut, Right(serviceTypesReturnEntityMock));
+  });
+  test(
+      'Deve chamar o datasource ao retornar uma lista de entidade correta ao fazer get do service types',
+      () async {
+    when(
+      () => getUserServiceTypesDatasourceMock.call(
+          serviceProviderId: createServiceTypeEntityMock),
+    ).thenAnswer((_) async => serviceTypesReturnEntityMock);
+    final sut = await getUserServiceTypesRepository.call(
+        serviceProviderId: createServiceTypeEntityMock);
+    expect(sut, Right(serviceTypesReturnEntityMock));
+    verify(
+      () => getUserServiceTypesDatasourceMock.call(
+          serviceProviderId: createServiceTypeEntityMock),
+    ).called(1);
+  });
+  test('Deve um erro do tipo correto ao fazer get do service types', () async {
+    when(
+      () => getUserServiceTypesDatasourceMock.call(
+          serviceProviderId: createServiceTypeEntityMock),
+    ).thenThrow(GetUserServiceTypesError(message: 'message'));
+    final sut = await getUserServiceTypesRepository.call(
+        serviceProviderId: createServiceTypeEntityMock);
+    expect(sut, Left(GetUserServiceTypesError(message: 'message')));
+  });
+}
