a7a8d011a936b2895d90ea5674763ab7f15f718c
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-27 16:27:29 -0300

    terminado testes para o repository de procurar service providers

---
+++ stool-in-app-ui/test/features/search/data/repository/search_service_provider_repository_impl_test.dart
@@ -0,0 +1,64 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:stool_in/exports/app_exports.dart';
+
+class _SearchServiceProvidersDatasourceMock extends Mock
+    implements SearchServiceProviderDatasource {}
+
+class _ServiceProviderReturnEntityMock extends Mock
+    implements ServiceProviderEntity {}
+
+void main() {
+  late _SearchServiceProvidersDatasourceMock serviceProvidersDatasourceMock;
+  late SearchServiceProviderRepository searchServiceProviderRepository;
+  late List<_ServiceProviderReturnEntityMock> serviceProviderEntityMockList;
+  late SearchServiceProviderQueryEntity serviceProviderEntityMock;
+  setUpAll(() {
+    serviceProviderEntityMock =
+        SearchServiceProviderQueryEntity(query: 'query');
+    serviceProviderEntityMockList = [_ServiceProviderReturnEntityMock()];
+    serviceProvidersDatasourceMock = _SearchServiceProvidersDatasourceMock();
+    searchServiceProviderRepository = SearchServiceProviderRepositoryImpl(
+        searchServiceProviderDatasource: serviceProvidersDatasourceMock);
+    registerFallbackValue(SearchServiceProviderModel.fromEntity(
+        searchServiceProviderQueryEntity: serviceProviderEntityMock));
+  });
+  test(
+      'Deve retornar uma lista de service providers ao fazer search no repository impl',
+      () async {
+    when(
+      () => serviceProvidersDatasourceMock.searchServiceProviders(
+          serviceProviderModel: any(named: 'serviceProviderModel')),
+    ).thenAnswer((_) async => serviceProviderEntityMockList);
+    final sut = await searchServiceProviderRepository.call(
+        searchServiceProviderQueryEntity: serviceProviderEntityMock);
+    expect(sut, Right(serviceProviderEntityMockList));
+  });
+  test(
+      'Deve chamar o datasource ao retornar uma lista de service providers ao fazer search no repository impl',
+      () async {
+    when(
+      () => serviceProvidersDatasourceMock.searchServiceProviders(
+          serviceProviderModel: any(named: 'serviceProviderModel')),
+    ).thenAnswer((_) async => serviceProviderEntityMockList);
+    final sut = await searchServiceProviderRepository.call(
+        searchServiceProviderQueryEntity: serviceProviderEntityMock);
+    expect(sut, Right(serviceProviderEntityMockList));
+    verify(
+      () => serviceProvidersDatasourceMock.searchServiceProviders(
+          serviceProviderModel: any(named: 'serviceProviderModel')),
+    ).called(1);
+  });
+  test(
+      'Deve retornar um erro ao tentar retornar uma lista de service providers ao fazer search no repository impl',
+      () async {
+    when(
+      () => serviceProvidersDatasourceMock.searchServiceProviders(
+          serviceProviderModel: any(named: 'serviceProviderModel')),
+    ).thenThrow(SearchServiceProviderError(message: 'message'));
+    final sut = await searchServiceProviderRepository.call(
+        searchServiceProviderQueryEntity: serviceProviderEntityMock);
+    expect(sut, Left(SearchServiceProviderError(message: 'message')));
+  });
+}
