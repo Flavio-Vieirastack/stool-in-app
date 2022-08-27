b1a9d19c76883d3115a1892d31ab56c6fdd67c8b
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-27 16:10:12 -0300

    terminado testes para usecase de pesquisa

---
+++ stool-in-app-ui/test/features/search/domain/usecase/search_service_provider_usecase_impl_test.dart
@@ -0,0 +1,63 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:stool_in/exports/app_exports.dart';
+
+class _SearchServiceProviderRepositoryMock extends Mock
+    implements SearchServiceProviderRepository {}
+
+class _EntityMock extends Mock implements SearchServiceProviderQueryEntity {}
+
+class _ServiceProviderEntityMock extends Mock implements ServiceProviderEntity {
+}
+
+void main() {
+  late _SearchServiceProviderRepositoryMock searchServiceProviderRepositoryMock;
+  late SerarchServiceProviderUsecase serarchServiceProviderUsecase;
+  late _EntityMock entityMock;
+  late List<_ServiceProviderEntityMock> serviceProviderEntityMock;
+  setUp(() {
+    searchServiceProviderRepositoryMock =
+        _SearchServiceProviderRepositoryMock();
+    serviceProviderEntityMock = [_ServiceProviderEntityMock()];
+    serarchServiceProviderUsecase = SearchServiceProviderUsecaseImpl(
+        searchServiceProviderRepository: searchServiceProviderRepositoryMock);
+    entityMock = _EntityMock();
+  });
+
+  test('Deve retornar uma lista de service providers ao fazer pesquisa',
+      () async {
+    when(
+      () => searchServiceProviderRepositoryMock.call(
+          searchServiceProviderQueryEntity: entityMock),
+    ).thenAnswer((_) async => Right(serviceProviderEntityMock));
+    final sut = await serarchServiceProviderUsecase.call(
+        searchServiceProviderQueryEntity: entityMock);
+    expect(sut, Right(serviceProviderEntityMock));
+  });
+  test('Deve chamar o repository ao retornar uma lista de service providers ao fazer pesquisa',
+      () async {
+    when(
+      () => searchServiceProviderRepositoryMock.call(
+          searchServiceProviderQueryEntity: entityMock),
+    ).thenAnswer((_) async => Right(serviceProviderEntityMock));
+    final sut = await serarchServiceProviderUsecase.call(
+        searchServiceProviderQueryEntity: entityMock);
+    expect(sut, Right(serviceProviderEntityMock));
+    verify(
+      () => searchServiceProviderRepositoryMock.call(
+          searchServiceProviderQueryEntity: entityMock),
+    ).called(1);
+  });
+  test('Deve retornar um erro ao tentar buscar uma lista de service providers ao fazer pesquisa',
+      () async {
+    when(
+      () => searchServiceProviderRepositoryMock.call(
+          searchServiceProviderQueryEntity: entityMock),
+    ).thenAnswer((_) async => Left(SearchServiceProviderError(message: '')));
+    final sut = await serarchServiceProviderUsecase.call(
+        searchServiceProviderQueryEntity: entityMock);
+    expect(sut, Left(SearchServiceProviderError(message: '')));
+   
+  });
+}
