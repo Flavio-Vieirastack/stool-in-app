5ecef432acfbace3858d87ebc8b15a84f97da671
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-28 16:10:00 -0300

    feito logica para as moedas

--- stool-in-app-ui/lib/features/home/presenter/page/widgets/coin_widget.dart
+++ stool-in-app-ui/lib/features/home/presenter/page/widgets/coin_widget.dart
@@ -6,10 +6,9 @@ Widget _coin({required VoidCallback onTap, required String coins}) {
     child: SizedBox(
       width: coins.length == 3 ? 10.w : 15.w,
       child: Text(
-        coins,
+        coins.length <= 5 ? coins : '${coins.substring(0,4)}+',
         softWrap: false,
         maxLines: 1,
-        overflow: TextOverflow.ellipsis,
         style: AppTextStyles.headLine3,
       ),
     ),
