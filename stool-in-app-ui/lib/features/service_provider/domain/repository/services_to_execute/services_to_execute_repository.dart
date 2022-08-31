59928db9ac0b45c01597326179ae1c224c39041a
Flavio Vieira <flavioemerson1@outlook.com>
2022-08-31 14:15:03 -0300

    criado repoisitory para services to execute

--- stool-in-app-ui/lib/features/service_provider/domain/repository/services_to_execute/services_to_execute_repository.dart
+++ stool-in-app-ui/lib/features/service_provider/domain/repository/services_to_execute/services_to_execute_repository.dart
@@ -1,6 +1,22 @@
 import 'package:dartz/dartz.dart';
 import 'package:stool_in/features/service_provider/domain/error/services_to_execute_error.dart';
 
+import '../../../../../exports/app_exports.dart';
+
 abstract class ServicesToExecuteRepository {
-  Future<Either<ServicesToExecuteError, void>> createServiceToExecute();
+  Future<Either<ServicesToExecuteError, void>> createServiceToExecute({
+    required ServicesToExecuteEntity servicesToExecuteEntity,
+  });
+  Future<Either<ServicesToExecuteError, void>> updateServicesToExecute({
+    required ServicesToExecuteEntity servicesToExecuteEntity,
+  });
+  Future<Either<ServicesToExecuteError, List<ServicesToExecuteEntity>>>
+      getAllServicesToExecute();
+  Future<Either<ServicesToExecuteError, ServicesToExecuteEntity>>
+      getServicesToExecuteUnique({
+    required int serviceToExecuteId,
+  });
+  Future<Either<ServicesToExecuteError, void>> deleteServiceToExecute({
+    required int serviceToExecuteid,
+  });
 }
