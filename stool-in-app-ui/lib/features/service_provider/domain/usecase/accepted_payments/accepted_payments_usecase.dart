ba6622be09d94093c85873e7425ba2c56629df8f
Flavio Vieira <flavioemerson1@outlook.com>
2022-09-03 14:12:04 -0300

    criado domain para accepted payments

--- stool-in-app-ui/lib/features/service_provider/domain/usecase/accepted_payments/accepted_payments_usecase.dart
+++ stool-in-app-ui/lib/features/service_provider/domain/usecase/accepted_payments/accepted_payments_usecase.dart
@@ -1,9 +1,10 @@
 import 'package:dartz/dartz.dart';
 import 'package:stool_in/features/home/domain/entity/service_provider/accepted_payments_entity.dart';
-import 'package:stool_in/features/service_provider/domain/error/accepted_payments_error.dart';
 
-abstract class ServiceProviderAcceptedPaymentsRepository {
-  Future<Either<AcceptedPaymentsError, void>> createAcceptedPayment({
+import '../../error/accepted_payments_error.dart';
+
+abstract class AcceptedPaymentsUsecase {
+Future<Either<AcceptedPaymentsError, void>> createAcceptedPayment({
     required int serviceProviderid,
     required AcceptedPaymentsEntity acceptedPaymentsEntity,
   });
---
+++ stool-in-app-ui/lib/features/service_provider/domain/usecase/accepted_payments/accepted_payments_usecase_impl.dart
@@ -0,0 +1,52 @@
+import 'package:stool_in/features/service_provider/domain/error/accepted_payments_error.dart';
+import 'package:stool_in/features/home/domain/entity/service_provider/accepted_payments_entity.dart';
+import 'package:dartz/dartz.dart';
+import 'package:stool_in/features/service_provider/domain/usecase/accepted_payments/accepted_payments_usecase.dart';
+
+import '../../repository/accepted_payments/service_provider_accepted_payments_repository.dart';
+
+class AcceptedPaymentsUsecaseImpl implements AcceptedPaymentsUsecase {
+  final ServiceProviderAcceptedPaymentsRepository
+      _serviceProviderAcceptedPaymentsRepository;
+  AcceptedPaymentsUsecaseImpl({
+    required ServiceProviderAcceptedPaymentsRepository
+        serviceProviderAcceptedPaymentsRepository,
+  }) : _serviceProviderAcceptedPaymentsRepository =
+            serviceProviderAcceptedPaymentsRepository;
+  @override
+  Future<Either<AcceptedPaymentsError, void>> createAcceptedPayment({
+    required int serviceProviderid,
+    required AcceptedPaymentsEntity acceptedPaymentsEntity,
+  }) async {
+    return await _serviceProviderAcceptedPaymentsRepository
+        .createAcceptedPayment(
+      serviceProviderid: serviceProviderid,
+      acceptedPaymentsEntity: acceptedPaymentsEntity,
+    );
+  }
+
+  @override
+  Future<Either<AcceptedPaymentsError, AcceptedPaymentsEntity>>
+      getAcceptedPaymentUnique({
+    required int paymentId,
+    required AcceptedPaymentsEntity acceptedPaymentsEntity,
+  }) async {
+    return await _serviceProviderAcceptedPaymentsRepository
+        .getAcceptedPaymentUnique(
+      paymentId: paymentId,
+      acceptedPaymentsEntity: acceptedPaymentsEntity,
+    );
+  }
+
+  @override
+  Future<Either<AcceptedPaymentsError, void>> updateAcceptedPayment({
+    required int paymentId,
+    required AcceptedPaymentsEntity acceptedPaymentsEntity,
+  }) async {
+    return await _serviceProviderAcceptedPaymentsRepository
+        .updateAcceptedPayment(
+      paymentId: paymentId,
+      acceptedPaymentsEntity: acceptedPaymentsEntity,
+    );
+  }
+}
