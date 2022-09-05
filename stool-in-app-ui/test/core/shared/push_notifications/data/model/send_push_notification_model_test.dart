9b311c7ffc92f5c8badb5aacf3b9bc1173268d66
Flavio Vieira <flavioemerson1@outlook.com>
2022-09-05 13:47:37 -0300

    terminado teste para model de push notification

---
+++ stool-in-app-ui/test/core/shared/push_notifications/data/model/send_push_notification_model_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stool_in/core/shared/push_notifications/data/model/send_push_notification_model.dart';
+
+void main() {
+  test('Deve retornar o valore correto para push token', () async {
+    final model = SendPushNotificationModel(
+            pushToken: 'pushToken', title: 'title', body: 'body')
+        .toMap();
+    final sut = model['pushToken'];
+    const matcher = 'pushToken';
+    expect(sut, matcher);
+  });
+  test('Deve retornar o valore correto para title', () async {
+    final model = SendPushNotificationModel(
+            pushToken: 'pushToken', title: 'title', body: 'body')
+        .toMap();
+    final sut = model['title'];
+    const matcher = 'title';
+    expect(sut, matcher);
+  });
+  test('Deve retornar o valore correto para body', () async {
+    final model = SendPushNotificationModel(
+            pushToken: 'pushToken', title: 'title', body: 'body')
+        .toMap();
+    final sut = model['body'];
+    const matcher = 'body';
+    expect(sut, matcher);
+  });
+}
