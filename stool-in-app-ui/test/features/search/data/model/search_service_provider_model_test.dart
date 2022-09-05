0f3701110d3a4d8cc0db135013917830bd6ddc42
Flavio Vieira <flavioemerson1@outlook.com>
2022-09-05 14:25:09 -0300

    feito teste para o model de search

--- stool-in-app-ui/test/features/search/data/model/search_service_provider_model_test.dart
+++ stool-in-app-ui/test/features/search/data/model/search_service_provider_model_test.dart
@@ -1,7 +1,21 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:stool_in/features/search/data/model/search_service_provider_model.dart';
 
 void main() {
-  testWidgets('search service provider model ...', (tester) async {
-    // TODO: Implement test
+  test('Deve retornar o valor correto para query', () async {
+    final model =
+        SearchServiceProviderModel(query: 'query', pages: 'pages').toMap();
+    final sut = model['search'];
+    const matcher = 'query';
+    expect(sut, matcher);
+    expect(sut, isA<String>());
+  });
+  test('Deve retornar o valor correto para pages', () async {
+    final model =
+        SearchServiceProviderModel(query: 'query', pages: 'pages').toMap();
+    final sut = model['pages'];
+    const matcher = 'pages';
+    expect(sut, matcher);
+    expect(sut, isA<String>());
   });
 }
