398cd84b9b4738d9544fa983d1f93a2d5bd52404
Flavio Vieira <flavioemerson1@outlook.com>
2022-09-07 17:15:58 -0300

    gerado exports do core

---
+++ stool_in_core/lib/export.dart
@@ -0,0 +1,4 @@
+// run this to reset your file: dart run build_runner build
+// remenber to format this file, you can use: dart format
+// publish your package hint: dart pub publish --dry-run
+// if you want to update your packages on power: dart pub upgrade --major-versions
