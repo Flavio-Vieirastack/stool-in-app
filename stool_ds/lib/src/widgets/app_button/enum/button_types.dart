95b6cae93673a4d531d2e2407034dc84642c6660
Flavio Vieira <flavioemerson1@outlook.com>
2022-09-08 17:10:22 -0300

    revertido enum de botão

--- stool_ds/lib/src/widgets/app_button/enum/button_types.dart
+++ stool_ds/lib/src/widgets/app_button/enum/button_types.dart
@@ -1,17 +1,6 @@
-import 'package:flutter/material.dart';
-
-import '../../../../stool_ds.dart';
-
 enum ButtonTypes {
   primary,
   secondary,
   disabled,
   loading,
-}
-
-enum Teste {
-  primary(colors: AppColors.black);
-
-  final Color colors;
-  const Teste({required this.colors});
 }
